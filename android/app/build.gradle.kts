746fbf4cf7dbcc043f24e130af5c333efbe5fee2
m3lloo <johnmelloregencia9@gmail.com>
2026-06-01 22:22:56 +0800

    fix(android): Set minSdk to 31 for Material Design 3 compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.example.flutterdev"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 31  // Required for Material Design 3
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
